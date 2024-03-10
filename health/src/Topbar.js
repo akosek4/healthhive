56dd6d88e6d842cff002c6261591bacce8698091
mlee60 <147071950+mlee60@users.noreply.github.com>
2024-03-09 18:15:06 -0800

    Changed the top header

--- health/src/Topbar.js
+++ health/src/Topbar.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 const Topbar = () => {
   return (
-    <div>
+    <div> 
         <h1 className='Topbar'>
-            Welcome to Health Hive
+            Welcome to Health Hive...
         </h1>
+        <div className = 'MiddleSection'>
         <h2 className = 'Parent'>
             <div className='Let'>
                 Let's talk about
@@ -14,6 +15,11 @@ const Topbar = () => {
                 Health
              </div>
         </h2>
+        <p className= 'Introduction'>
+        Within these virtual walls, we offer a supportive community where individuals can share their experiences anonymously under specific diagnoses. Navigating health challenges can be isolating, and we recognize the importance of providing a safe space for open dialogue. Here, your journey is valid, your voice matters, and you are not alone. Health Hive is more than a forum; it's a collective embrace of shared wisdom, empathy, and healing. Join us as we foster connections, erase judgment, and build a hive of support that transcends the boundaries of diagnosis, creating a space where every story contributes to a tapestry of strength and resilience.
+        </p>
+        
+        </div>
     </div>
   )
 }
