d6187f3f41d8b67b93048ce1ad37b2573fcaa369
sabrinawoo <sabrinawoo3895@gmail.com>
2024-03-09 22:34:29 -0800

    add comment functionality

--- health/src/App.js
+++ health/src/App.js
@@ -1,38 +1,30 @@
-import { useState } from 'react';
+
 import './App.css';
 import Disorders from './Disorders';
 import Topbar from './Topbar';
 
 
+
 function App() {
-  const [disorders, setDisorders] = useState(
-    [
-      {
-        id: 1, 
-        name: "Depression",
-        text: "prolonged sadness and lack of motivation",
-        comments: [
-          'a walk every day helps',
-          'i was misdiagnosed twice',
-          'you guys are so strong!',
-          'my doctor just increased my dosage!!']
-      }, 
-      {
-        id: 2, 
-        name: "ADHD",
-        text: "difficulties focusing",
-        comments: ['i feel like its rlly affecting my grades :(',
-        'i heard mushrooms are good for focusing']
-      }
-    ]
-  )
+
+
+  // const onAdd = () => {
+  //   set
+  // }
+
+// //add comment
+// const addComment = (string) => {
+//   const id=Math.floor(Math.random()*10000+1)
+//   const newTask = {id, ...task}
+//   //sets task as the current array plus the new one
+//   setTasks([...tasks, newTask])
+
+// }
 
   return (
     <div>
       <Topbar/>
-      <Disorders 
-      disorders={disorders}
-       />
+      <Disorders />
     </div>
   );
 }
